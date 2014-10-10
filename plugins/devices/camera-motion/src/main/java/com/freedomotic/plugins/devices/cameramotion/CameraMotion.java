ffb59cc2d9faf4df05303549592a4dbd2bd72487
Mauro Cicolella <mcicolella@libero.it>
2014-10-10 18:10:29 +0200

    camera motion plugin added motion detection and images capture

--- plugins/devices/camera-motion/src/main/java/com/freedomotic/plugins/devices/cameramotion/CameraMotion.java
+++ plugins/devices/camera-motion/src/main/java/com/freedomotic/plugins/devices/cameramotion/CameraMotion.java
@@ -22,22 +22,27 @@ package com.freedomotic.plugins.devices.cameramotion;
 import com.freedomotic.api.EventTemplate;
 import com.freedomotic.api.Protocol;
 import com.freedomotic.app.Freedomotic;
+import com.freedomotic.exceptions.PluginStartupException;
 import com.freedomotic.exceptions.UnableToExecuteException;
 import com.freedomotic.reactions.Command;
 import com.freedomotic.util.Info;
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.*;
 import com.github.sarxos.webcam.ds.ipcam.*;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.border.Border;
 
 public class CameraMotion
         extends Protocol {
@@ -47,12 +52,10 @@ public class CameraMotion
     static {
         Webcam.setDriver(new IpCamDriver(new IpCamStorage(Info.PATHS.PATH_DEVICES_FOLDER + "/camera-motion/" + "cameras.xml")));
     }
-    final int POLLING_WAIT;
     JFrame f = new JFrame("Camera Motion");
 
     public CameraMotion() {
         super("Camera Motion", "/camera-motion/camera-motion-manifest.xml");
-        POLLING_WAIT = configuration.getIntProperty("time-between-reads", 2000);
         setPollingWait(-1);
     }
 
@@ -63,21 +66,21 @@ public class CameraMotion
 
     @Override
     protected void onHideGui() {
-        //implement here what to do when the this plugin GUI is closed
-        //for example you can change the plugin description
         setDescription("Camera Motion");
     }
 
     @Override
     protected void onRun() {
+        new DetectMotion();
     }
 
     @Override
-    protected void onStart() {
+    protected void onStart() throws PluginStartupException {
         try {
             loadCameras();
         } catch (MalformedURLException ex) {
-            Logger.getLogger(CameraMotion.class.getName()).log(Level.SEVERE, null, ex);
+            // TODO if there are no cameras available stop plugin
+            throw new PluginStartupException("Error loading cameras " + ex.getMessage(), ex);
         }
         LOG.info("Camera Motion plugin started");
 
@@ -112,11 +115,14 @@ public class CameraMotion
         f.setLayout(new GridLayout(0, 3, 1, 1));
         List<WebcamPanel> panels = new ArrayList<WebcamPanel>();
         for (Webcam webcam : Webcam.getWebcams()) {
+            // TODO set panel dimensions as config parameters
             WebcamPanel panel = new WebcamPanel(webcam, new Dimension(256, 144), false);
             panel.setFillArea(true);
             panel.setFPSLimited(true);
             panel.setFPSLimit(0.2); // 0.1 FPS = 1 frame per 10 seconds
-            panel.setBorder(BorderFactory.createEmptyBorder());
+            //panel.setBorder(BorderFactory.createEmptyBorder());
+            Border title = BorderFactory.createTitledBorder(webcam.getName());
+            panel.setBorder(title);
             f.add(panel);
             panels.add(panel);
         }
@@ -125,5 +131,58 @@ public class CameraMotion
         for (WebcamPanel panel : panels) {
             panel.start();
         }
+    }
+
+    public class DetectMotion implements WebcamMotionListener {
+
+        List<WebcamMotionDetector> detectors = new ArrayList<WebcamMotionDetector>();
+
+        public DetectMotion() {
+            for (Webcam webcam : Webcam.getWebcams()) {
+                WebcamMotionDetector detector = new WebcamMotionDetector(webcam);
+                detector.setInterval(100); // one check per 100 ms (10 FPS)
+                detector.addMotionListener(this);
+                detectors.add(detector);
+            }
+            for (WebcamMotionDetector detector : detectors) {
+                detector.start();
+            }
+        }
+
+        @Override
+        public void motionDetected(WebcamMotionEvent wme) {
+            Webcam webcam = ((WebcamMotionDetector) wme.getSource()).getWebcam();
+            IpCamDevice device = (IpCamDevice) webcam.getDevice(); // in case whe IP camera driver is used
+            System.out.println("Camera " + webcam.getName() + " detected motion!");
+            // capture an image
+            captureImage(webcam.getName());
+            //System.out.println("Webcam name: " + webcam.getName());
+            //System.out.println("Webcam URL: " + device.getURL());
+        }
+    }
+
+    public void captureImage(String cameraName) {
+        Webcam webcam = getCameraByName(cameraName);
+        if (webcam != null) {
+            webcam.open();
+            BufferedImage image = webcam.getImage();
+            Date now = new Date();
+            SimpleDateFormat dateformat =
+                    new SimpleDateFormat("dd MMMM yyyy - HH:mm.ss");
+            try {
+                ImageIO.write(image, "PNG", new File(Info.PATHS.PATH_DEVICES_FOLDER + "/camera-motion/data/" + webcam.getName() + "_" + now + ".png"));
+            } catch (IOException ex) {
+                LOG.severe(ex.getMessage());
+            }
+        }
+    }
+
+    public Webcam getCameraByName(String name) {
+        for (Webcam webcam : Webcam.getWebcams()) {
+            if (webcam.getName().equalsIgnoreCase(name)) {
+                return webcam;
+            }
+        }
+        return null;
     }
 }
